0e0963d257c2de130c509d4024b6e9bc3504daf2
lzm <lzm@163.com>
2018-11-19 21:28:22 +0800

    提交代码

---
+++ gitdemo/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,11 @@
+package com.itheima.domain;
+
+/**
+ * @package: com.itheima.domain
+ * @Description:
+ * @author: Administrator
+ * @date 2018/11/19  21:25
+ */
+public class User {
+    private String name;
+}
